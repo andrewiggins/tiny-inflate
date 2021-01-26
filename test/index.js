b91f68496a3e792d69087d789b6737cf186d0124
Andre Wiggins <andrewiggins@live.com>
2021-01-25 22:22:13 -0800

    Convert test suite to uvu for easier node debugging

--- test/index.js
+++ test/index.js
@@ -4,84 +4,73 @@ import { fileURLToPath } from 'url';
 import { createDeflateRaw, deflateRaw, constants } from 'zlib';
 import { readFileSync } from 'fs';
 import { deepStrictEqual, strictEqual } from 'assert';
+import { promisify } from 'util';
+import { test } from 'uvu';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const uncompressed = readFileSync(__dirname + '/lorem.txt');
 
-describe('tiny-inflate', function () {
-  let compressed, noCompression, fixed;
+let compressed, noCompression, fixed;
 
-  function deflate(buf, options, fn) {
+function deflate(buf, options) {
+  return new Promise((resolve, reject) => {
     const chunks = [];
     createDeflateRaw(options)
-      .on('data', function (chunk) {
+      .on('data', (chunk) => {
         chunks.push(chunk);
       })
-      .on('error', fn)
-      .on('end', function () {
-        fn(null, Buffer.concat(chunks));
+      .on('error', reject)
+      .on('end', () => {
+        resolve(Buffer.concat(chunks));
       })
       .end(buf);
-  }
-
-  before(function (done) {
-    deflateRaw(uncompressed, function (err, data) {
-      compressed = data;
-      done();
-    });
   });
+}
 
-  before(function (done) {
-    deflate(
-      uncompressed,
-      { level: constants.Z_NO_COMPRESSION },
-      function (err, data) {
-        noCompression = data;
-        done();
-      }
-    );
-  });
+test.before(async () => {
+  compressed = await promisify(deflateRaw)(uncompressed);
+});
 
-  before(function (done) {
-    deflate(
-      uncompressed,
-      { strategy: constants.Z_FIXED },
-      function (err, data) {
-        fixed = data;
-        done();
-      }
-    );
-  });
-
-  it('should inflate some data', function () {
-    let out = Buffer.alloc(uncompressed.length);
-    inflate(compressed, out);
-    deepStrictEqual(out, uncompressed);
-  });
-
-  it('should slice output buffer', function () {
-    let out = Buffer.alloc(uncompressed.length + 1024);
-    let res = inflate(compressed, out);
-    deepStrictEqual(res, uncompressed);
-    strictEqual(res.length, uncompressed.length);
-  });
-
-  it('should handle uncompressed blocks', function () {
-    let out = Buffer.alloc(uncompressed.length);
-    inflate(noCompression, out);
-    deepStrictEqual(out, uncompressed);
-  });
-
-  it('should handle fixed huffman blocks', function () {
-    let out = Buffer.alloc(uncompressed.length);
-    inflate(fixed, out);
-    deepStrictEqual(out, uncompressed);
-  });
-
-  it('should handle typed arrays', function () {
-    let input = new Uint8Array(compressed);
-    let out = new Uint8Array(uncompressed.length);
-    inflate(input, out);
-    deepStrictEqual(out, new Uint8Array(uncompressed));
+test.before(async () => {
+  noCompression = await deflate(uncompressed, {
+    level: constants.Z_NO_COMPRESSION,
   });
 });
+
+test.before(async () => {
+  fixed = await deflate(uncompressed, { strategy: constants.Z_FIXED });
+});
+
+test('should inflate some data', () => {
+  let out = Buffer.alloc(uncompressed.length);
+  inflate(compressed, out);
+  deepStrictEqual(out, uncompressed);
+});
+
+test('should slice output buffer', () => {
+  let out = Buffer.alloc(uncompressed.length + 1024);
+  let res = inflate(compressed, out);
+  deepStrictEqual(res, uncompressed);
+  strictEqual(res.length, uncompressed.length);
+});
+
+test('should handle uncompressed blocks', () => {
+  let out = Buffer.alloc(uncompressed.length);
+  inflate(noCompression, out);
+  deepStrictEqual(out, uncompressed);
+});
+
+test('should handle fixed huffman blocks', () => {
+  let out = Buffer.alloc(uncompressed.length);
+  inflate(fixed, out);
+  deepStrictEqual(out, uncompressed);
+});
+
+test('should handle typed arrays', () => {
+  let input = new Uint8Array(compressed);
+  let out = new Uint8Array(uncompressed.length);
+  inflate(input, out);
+  deepStrictEqual(out, new Uint8Array(uncompressed));
+});
+
+test.run();
